a54171dcf2eb00f85b8d453b1e09624108b16b97
xianzi.zeng <xianzi.zeng>
2018-12-28 09:42:59 +0800

    EditText

--- sharpview/src/main/java/com/zhaoxing/view/sharpview/SharpEditText.java
+++ sharpview/src/main/java/com/zhaoxing/view/sharpview/SharpEditText.java
@@ -1,15 +1,14 @@
 package com.zhaoxing.view.sharpview;
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
-
-import com.rey.material.widget.EditText;
 
 /**
  * Created by 曾宪梓 on 2017/12/28.
  */
 
-public class SharpEditText extends EditText implements SharpView{
+public class SharpEditText extends AppCompatEditText implements SharpView{
 
     public SharpEditText(Context context) {
         super(context);
